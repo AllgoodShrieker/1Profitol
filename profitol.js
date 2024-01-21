ddded40e50edb6ca9e4772157a0cfe9c267f3552
AllgoodShrieker <157093606+AllgoodShrieker@users.noreply.github.com>
2024-01-21 21:04:39 +0530

    profitol.js

---
+++ profitol.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: ["Engineer", "software Developer", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+})
